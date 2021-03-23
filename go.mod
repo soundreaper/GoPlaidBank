4b3af93d2eeb2d2ad548170e3daad5fc5b4edce6
soundreaper@gmail.com <soundreaper@gmail.com>
2021-03-23 14:39:48 +0545

    add plaid key to env file & initialize variables for plaid API client

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module server
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.6.3 // indirect
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/joho/godotenv v1.3.0
+	github.com/plaid/plaid-go v0.0.0-20210216195344-700b8cfc627d
+)
--- server.go
+++ server.go
@@ -1,8 +1,47 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/plaid/plaid-go/plaid"
 )
+
+var (
+	PLAID_CLIENT_ID                   = ""
+	PLAID_SECRET                      = ""
+	PLAID_ENV                         = ""
+	PLAID_PRODUCTS                    = ""
+	PLAID_COUNTRY_CODES               = ""
+	PLAID_REDIRECT_URI                = ""
+	APP_PORT                          = ""
+	client              *plaid.Client = nil
+)
+
+var environments = map[string]plaid.Environment{
+	"sandbox":     plaid.Sandbox,
+	"development": plaid.Development,
+	"production":  plaid.Production,
+}
+
+func init() {
+	err := godotenv.Load()
+
+	PLAID_CLIENT_ID = os.Getenv("PLAID_CLIENT_ID")
+	PLAID_SECRET = os.Getenv("PLAID_SECRET")
+
+	if PLAID_CLIENT_ID == "" || PLAID_SECRET == "" {
+		log.Fatal("Error: PLAID_SECRET or PLAID_CLIENT_ID is not set. Did you copy .env.example to .env and fill it out?")
+	}
+
+	PLAID_ENV = os.Getenv("PLAID_ENV")
+	PLAID_PRODUCTS = os.Getenv("PLAID_PRODUCTS")
+	PLAID_COUNTRY_CODES = os.Getenv("PLAID_COUNTRY_CODES")
+	PLAID_REDIRECT_URI = os.Getenv("PLAID_REDIRECT_URI")
+	APP_PORT = os.Getenv("APP_PORT")
+}
 
 func main() {
 	r := gin.Default()
